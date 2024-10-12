9d31753a9543e852016995b54e54e347b56f8131
Mike Job <mikejobrn@gmail.com>
2024-10-12 14:03:42 -0300

    Aidiciona suporte ao HttpClient e a recursos de template (como AlertController) na versão standalone

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot({ innerHTMLTemplatesEnabled: true })),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withInterceptorsFromDi())
   ],
 });
